52bfc7c82fedb2bff4deb0b1c05655223ce46167
imakash-24 <imakash2409@gmail.com>
2025-09-29 12:49:06 +0530

    env file changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async ()=> {
-    await mongoose.connect("mongodb+srv://jaiakash2409:Akashatlas24@cluster0.c1koewq.mongodb.net/Taskflow")
+    await mongoose.connect(process.env.MONGO_URI)
     .then(()=> console.log("DB connected"));
     
 }
