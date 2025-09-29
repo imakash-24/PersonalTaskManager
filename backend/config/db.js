3dc1233f3c4e0f13493970d1d3dbbc9938a69af6
imakash-24 <imakash2409@gmail.com>
2025-09-29 14:16:05 +0530

    db changesk

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
 
-export const connectDB = async ()=> {
-    await mongoose.connect(process.env.MONGO_URI)
-    .then(()=> console.log("DB connected"));
-    
+export const connectDB = async () => {
+    await mongoose.connect(process.env.MONGO_URI, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        tls: true 
+    })
+    .then(() => console.log("DB connected"));
 }
