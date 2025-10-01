2b998ce5e71a6f884d9b4f95ba4f6a19d6f4196d
imakash-24 <imakash2409@gmail.com>
2025-10-01 14:45:46 +0530

    CROS changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express()
 const port = process.env.PORT || 4000
 
 //Middleware
-app.use(cors())
+app.use(cors({
+  origin: "https://personaltaskmanager-frontend-3w6u.onrender.com", // allow only your frontend
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
